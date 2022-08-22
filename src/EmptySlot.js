2ea219837d2b27df69dfc4757005a4849742ec2d
Crystal Korth <crystalkorth@gmail.com>
2022-08-22 17:37:14 -0500

    initial commit

---
+++ src/EmptySlot.js
@@ -0,0 +1,12 @@
+// import Mole from './Mole'
+
+function EmptySlot(props){
+    return (
+        <div>
+            <h2> Empty Slot </h2>
+            <Mole />
+        </div>
+    )
+}
+
+export default EmptySlot
---
+++ src/Mole.js
@@ -0,0 +1,12 @@
+// import Mole from './Mole'
+
+function Mole(props){
+    return (
+        <div>
+            <h2> Mole </h2>
+            <Mole />
+        </div>
+    )
+}
+
+export default Mole
